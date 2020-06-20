839a7d1e66d04973cb8aa01ece88baa022f91c10
VishwaMishra <vishwabeit20@gmail.com>
2020-06-20 14:12:13 +0530

    Created Navigation Bar and update login page.

--- RBapp/app.py
+++ RBapp/app.py
@@ -2,10 +2,15 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def home():
-    return render_template("index.html")
+    name = ''
+    if request.method == "POST" and "fname" in request.form:
+        name= request.form.get('fname')
+    return render_template("index.html",name=name)
 
 @app.route("/login")
 def login():
 	return render_template("login.html")
+
+app.run(debug=True)
